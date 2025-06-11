e01eda6aa9c04b2540bacc6ab13594da88c5d50c
nkiran89 <kiran2272@gmail.com>
2025-06-11 18:31:16 +0530

    Create OSPlugin.js

---
+++ www/OSPlugin.js
@@ -0,0 +1,10 @@
+
+const exec = require('cordova/exec');
+
+let OSPlugin = {
+    showToast: (message, duration, successCallback, errorCallback) => {
+        exec(successCallback, errorCallback, 'OSPlugin', 'showToast', [message, duration]);
+    }
+};
+
+module.exports = OSPlugin;
